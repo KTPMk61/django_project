f0bc557d39af546f79d950d3bb3735828e960fe8
hoangbkcntt98 <20166135@student.hust.edu.vn>
2018-04-14 10:23:44 +0700

    Them field trong Student

--- test2/home/forms.py
+++ test2/home/forms.py
@@ -4,6 +4,9 @@ from .models import Student
 class RegistrationForm(forms.Form):
     fullname = forms.CharField(label = 'Họ Tên', max_length= 30)
     sid = forms.CharField(label='MSSV', max_length=30)
+    khoa = forms.CharField(label='Khoa/Vien',max_length=30)
+    email = forms.CharField(label='Emails/SDT',max_length=30)
+    bir = forms.DateTimeField(label='Ngay Sinh',input_formats=['%d/%m/%Y'])
     username = forms.CharField(label = 'Tài khoản', max_length= 30)
     password =  forms.CharField(label= 'Mật khẩu',widget= forms.PasswordInput())
     def save(self):
@@ -11,7 +14,10 @@ class RegistrationForm(forms.Form):
         username = self.cleaned_data['username']
         password = self.cleaned_data['password']
         sid = self.cleaned_data['sid']
-        s = Student(username = username, password = password, fullname= fullname,score= 0,sid= sid)
+        khoa = self.cleaned_data['khoa']
+        email = self.cleaned_data['email']
+        bir = self.cleaned_data['bir']
+        s = Student(username = username, password = password, fullname= fullname,score= 0,sid= sid,khoa= khoa,bir=bir,email= email)
         s.save()
 class LoginForm(forms.Form):
     username = forms.CharField(label = 'Tài khoản', max_length= 30)
--- test2/home/models.py
+++ test2/home/models.py
@@ -5,6 +5,9 @@ class Student(models.Model):
     fullname = models.CharField(max_length= 100)
     score = models.IntegerField()
     sid = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    khoa = models.CharField(max_length=30)
+    bir = models.DateTimeField()
     def __str__(self):
         return self.fullname
     def __str__(self):
